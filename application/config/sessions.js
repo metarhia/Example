a279a101a658492cdf6339c312e81d32ab1b3118
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-05-23 15:42:46 +0300

    Add configs
    PR-URL: https://github.com/metarhia/StarterKit/pull/4

---
+++ application/config/sessions.js
@@ -0,0 +1,14 @@
+({
+  // Sessions configuration
+  anonymous: true, // Allow anonymous sessions
+  cookie: 'SID', // Session cookie name
+  characters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+  secret: 'secret', // Session secret
+  length: 64, // SID length in bytes
+  persist: true, // Store sessions in persistent database
+  perIpLimit: 20,
+  perUserLimit: 5,
+  regenerate: 1 * 60 * 60 * 1000,
+  expire: 2 * 60 * 60 * 1000,
+  // domain: 'name.com' // domain for cookie
+});
