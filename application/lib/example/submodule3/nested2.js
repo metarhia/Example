f4c3f5dc030fdc259380de90c214669bdd0f930c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-08-16 22:07:46 +0300

    Examples for new loader
    Refs: https://github.com/metarhia/impress/issues/1270
    PR-URL: https://github.com/metarhia/Example/pull/24

---
+++ application/lib/example/submodule3/nested2.js
@@ -0,0 +1,5 @@
+({
+  method1() {
+    console.log('Call method: example.submodule3.nested2.method1');
+  },
+});
--- application/lib/resmon.js
+++ application/lib/resmon.js
@@ -1,14 +1,6 @@
 ({
-  getStatistics() {
-    const { heapTotal, heapUsed, external } = api.process.memoryUsage();
-    const hs = api.v8.getHeapStatistics();
-    const contexts = hs.number_of_native_contexts;
-    const detached = hs.number_of_detached_contexts;
-    return { heapTotal, heapUsed, external, contexts, detached };
-  },
-
-  start() {
-    const { interval } = application.resmon.config;
+  async start() {
+    const { interval } = this.config;
     setInterval(() => {
       const stats = application.resmon.getStatistics();
       const { heapTotal, heapUsed, external, contexts, detached } = stats;
@@ -18,5 +10,13 @@
       console.log(`Heap: ${used} of ${total}, ext: ${ext}`);
       console.log(`Contexts: ${contexts}, detached: ${detached}`);
     }, interval);
+  },
+
+  getStatistics() {
+    const { heapTotal, heapUsed, external } = api.process.memoryUsage();
+    const hs = api.v8.getHeapStatistics();
+    const contexts = hs.number_of_native_contexts;
+    const detached = hs.number_of_detached_contexts;
+    return { heapTotal, heapUsed, external, contexts, detached };
   }
 });
