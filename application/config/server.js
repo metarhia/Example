3750d4dbdc9399b5a6158596b042bf05d8860eac
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-08-26 11:49:48 +0300

    Workers: thread per port and thread pool
    PR-URL: https://github.com/metarhia/Example/pull/30

--- application/config/server.js
+++ application/config/server.js
@@ -1,10 +1,16 @@
 ({
   host: '127.0.0.1',
-  ports: [8000, 8001, 8002, 8003],
+  balancer: 8000,
+  protocol: 'http',
+  ports: [8001, 8002],
   timeout: 5000,
   concurrency: 1000,
   queue: {
     size: 2000,
+    timeout: 3000,
+  },
+  workers: {
+    pool: 2,
     timeout: 3000,
   }
 });
