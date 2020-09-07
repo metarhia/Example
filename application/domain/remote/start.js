706db54397a99b0f7a69bb5f5f789eb05e79fd84
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-09-07 04:06:15 +0300

    Wait for server start

--- application/domain/remote/start.js
+++ application/domain/remote/start.js
@@ -1,4 +1,6 @@
 (async () => {
+  // Wait for server start
+  await new Promise(resolve => setTimeout(resolve, 100));
   console.debug('Connect to metacom');
   const { url } = config.remote;
   const Metacom = npm.metacom;
