e0cc6a3598ab4627bee5f991f18e30d1f0147163
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-09-23 02:34:24 +0300

    Update to impress 2.0.0-alpha.27 add use cases
    Refs: https://github.com/metarhia/impress/pull/1351
    
    PR-URL: https://github.com/metarhia/Example/pull/40

--- application/lib/example/start.js
+++ application/lib/example/start.js
@@ -1,4 +1,10 @@
-(async () => {
-  console.debug('Start example plugin');
-  console.debug(config.example);
+({
+  privateField: 100,
+
+  async method() {
+    console.log('Start example plugin');
+    this.parent.cache.set({ key: 'keyName', val: this.privateField });
+    const res = lib.example.cache.get({ key: 'keyName' });
+    console.log({ res, cache: this.parent.cache.values });
+  }
 });
