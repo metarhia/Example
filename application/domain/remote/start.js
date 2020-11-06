26a7589c62d8e769d49fcccfcba9966c772b4b87
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-11-06 19:43:15 +0200

    fixup! Use external database from metasql module

--- application/domain/remote/start.js
+++ application/domain/remote/start.js
@@ -1,7 +1,7 @@
 (async () => {
   // Wait for server start
   await new Promise(resolve => setTimeout(resolve, 100));
-  console.debug('Connect to metacom';
+  console.debug('Connect to metacom');
   const { url } = config.remote;
   const Metacom = npm.metacom;
   const metacom = new Metacom(url);
