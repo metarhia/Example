d5e7338d0f1a433a7a7a782c142d93cec03fe19c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-12-07 02:37:27 +0200

    Update dependencies and configuration, publish 0.0.11
    Refs: https://github.com/metarhia/impress/issues/1406
    
    PR-URL: https://github.com/metarhia/Example/pull/60

--- application/config/log.test.js
+++ application/config/log.test.js
@@ -2,8 +2,6 @@
   keepDays: 100, // Delete files after N days
   writeInterval: 3000, // Flush log to disk interval
   writeBuffer: 64 * 1024, // Buffer size 64kb
-  toFile: [
-    'system', 'fatal', 'error', 'warn', 'info', 'debug', 'access', 'slow'
-  ],
-  toStdout: ['system', 'fatal', 'error', 'warn', 'debug']
+  toFile: ['error', 'warn', 'info', 'debug', 'log'],
+  toStdout: ['error', 'warn', 'info', 'debug', 'log']
 });
