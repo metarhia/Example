c589da24b10fa312aec03751a6701036f2a8f85f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-02-04 03:20:16 +0200

    Regenerate database structure

--- application/db/data.sql
+++ application/db/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO SystemUser (Login, Password) VALUES
+INSERT INTO "SystemUser" ("login", "password") VALUES
   ('admin', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$XcD5Zfk+BVIGEyiksBjjy9LL42AFOOqlhEB650woECs$3CNOs25gOVV8AZMYQc6bFnrYdM+3xP996shxJEq5LxGt4gs1g9cocZmi/SYg/H16egY4j7qxTD/oygyEI80cgg'),
   ('marcus', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$aGKuH5D2zndi6zFu74/rEj5m3u5kRh5b+QXYfKrhAU8$257up1h/3R9CoxH2382zX0+kbxRPrd+GwzJIxYI+K+gBYCcLrA8Z6wv7lSwLElfbDTJRgUhQJFhMT1tpp5AJxw'),
   ('user', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$z5uf2xGdpgq5v2sZbgh36QoZG9CDmGmJUNJkrs1zs2w$3s3x22k4Td0jkup4WduFQGFVjrFKHjN1WV9k8/Bh3DKI58Wrlo/D4Js9j/DiskwI8rltDd8pF15JylivFu2T0g'),
@@ -10,24 +10,24 @@ INSERT INTO SystemUser (Login, Password) VALUES
 -- user/nopassword
 -- iskandar/zulqarnayn
 
-INSERT INTO SystemGroup (Name) VALUES
+INSERT INTO "SystemGroup" ("name") VALUES
   ('admins'),
   ('users'),
   ('guests');
 
-INSERT INTO GroupUser (GroupId, UserId) VALUES
+INSERT INTO "SystemGroupSystemUser" ("systemGroupId", "systemUserId") VALUES
   (1, 1),
   (2, 2),
   (2, 3),
   (2, 4);
 
-INSERT INTO Country (Name) VALUES
+INSERT INTO "Country" ("name") VALUES
   ('Soviet Union'),
   ('People''s Republic of China'),
   ('Vietnam'),
   ('Cuba');
 
-INSERT INTO City (Name, CountryId) VALUES
+INSERT INTO "City" ("name", "countryId") VALUES
   ('Beijing', 2),
   ('Wuhan', 2),
   ('Kiev', 1),
