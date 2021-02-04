d166cb6d126935f676148afd1bb6c79c2089dfd7
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-02-04 03:22:05 +0200

    Fix database fields names
    PR-URL: https://github.com/metarhia/Example/pull/81

--- application/api/example.1/countries.js
+++ application/api/example.1/countries.js
@@ -1,5 +1,5 @@
 async () => {
-  const fields = ['Id', 'Name'];
+  const fields = ['countryId', 'name'];
   const data = await domain.db.select('Country', fields);
   return { result: 'success', data };
 };
