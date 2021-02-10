aa4ab26fcc806bcbdca6d8f0aea73b2013d6a333
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-02-10 10:03:56 +0200

    Now redis connection is optional
    Closes: https://github.com/metarhia/Example/issues/83
    
    PR-URL: https://github.com/metarhia/Example/pull/88

--- application/lib/redis/start.js
+++ application/lib/redis/start.js
@@ -1,4 +1,13 @@
 async () => {
-  console.log('Connect to redis');
-  lib.redis.client = npm.redis.createClient();
+  if (application.worker.id === 'W1') {
+    console.log('Connect to redis');
+  }
+  const client = npm.redis.createClient();
+  lib.redis.client = client;
+  client.on('error', () => {
+    if (application.worker.id === 'W1') {
+      console.log('No redis service detected, so quit client');
+    }
+    client.quit();
+  });
 };
