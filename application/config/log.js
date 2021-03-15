90c2ee32436d9d4203c0f65b7c9eee9b9f8fe677
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-16 00:26:14 +0200

    Update schemas to be compatible with impress 1.2.0

--- application/config/log.js
+++ application/config/log.js
@@ -3,5 +3,5 @@
   writeInterval: 3000,
   writeBuffer: 64 * 1024,
   toFile: ['error', 'warn', 'info', 'debug', 'log'],
-  toStdout: ['error', 'warn', 'info', 'debug', 'log']
+  toStdout: ['error', 'warn', 'info', 'debug', 'log'],
 });
