f7a1d2ebb101f2d4edab56c0f606521fad5a594c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-16 00:26:14 +0200

    Update schemas to be compatible with impress 1.2.0
    PR--URL: https://github.com/metarhia/Example/pull/101

--- application/config/server.js
+++ application/config/server.js
@@ -3,7 +3,14 @@
   balancer: 8000,
   protocol: 'http',
   ports: [8001, 8002],
-  timeout: 5000,
+  nagle: false,
+  timeouts: {
+    bind: 2000,
+    start: 30000,
+    stop: 5000,
+    request: 5000,
+    watch: 1000,
+  },
   queue: {
     concurrency: 1000,
     size: 2000,
@@ -11,6 +18,5 @@
   },
   workers: {
     pool: 2,
-    timeout: 3000,
-  }
+  },
 });
