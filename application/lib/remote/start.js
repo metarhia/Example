758e6e49b04c17af9a594d6ce932c3611efb71e5
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-04-27 21:26:34 +0300

    Move metacom example to lib and remove waiting for start

--- application/lib/remote/start.js
+++ application/lib/remote/start.js
@@ -1,12 +1,10 @@
 async () => {
-  // Wait for server start
-  await new Promise((resolve) => setTimeout(resolve, 100));
   if (application.worker.id === 'W1') {
     console.debug('Connect to metacom');
   }
   const Metacom = metarhia.metacom;
   const metacom = new Metacom(config.remote.url);
-  domain.remote.metacom = metacom;
+  lib.remote.metacom = metacom;
   metacom.socket.on('error', () => {
     if (application.worker.id === 'W1') {
       console.warn(`Can not connect to ${metacom.url}`);
