6803d4ea625cc25f157fda848aaa2c77745c7cd7
Dmytro Nechai <nechaido@gmail.com>
2021-04-30 13:16:47 +0300

    Fix type checking
    PR-URL: https://github.com/metarhia/Example/pull/116

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,3 +1,10 @@
+/* This import should not be removed. We need to reference impress explicitly
+ * so that tsc correctly resolved global variables.
+ * For some odd reason using typeRoots results in an array of errors.
+ * The problem should have been fixed by having an index file but no luck.
+ * PR with the correct fix would be greatly appreciated. */
+import * as _impress from 'impress';
+
 import * as _metasql from 'metasql';
 
 declare global {
