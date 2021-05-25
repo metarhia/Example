74cf78208ffc8913572560668d493abc201e9fae
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-05-25 15:17:20 +0300

    Remove unused examples

--- application/lib/resmon/getStatistics.js
+++
@@ -1,7 +0,0 @@
-() => {
-  const { heapTotal, heapUsed, external } = node.process.memoryUsage();
-  const hs = node.v8.getHeapStatistics();
-  const contexts = hs.number_of_native_contexts;
-  const detached = hs.number_of_detached_contexts;
-  return { heapTotal, heapUsed, external, contexts, detached };
-};
