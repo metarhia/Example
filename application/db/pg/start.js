2dc813ab10b9f5a85fb392c4e9c7e093f751d5f7
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-07-29 10:41:47 +0300

    Move db connections to `application/db`

--- application/db/pg/start.js
+++ application/db/pg/start.js
@@ -3,6 +3,5 @@ async () => {
     console.debug('Connect to pg');
   }
   const options = { ...config.database, logger: console };
-  const database = new metarhia.metasql.Database(options);
-  domain.db = database;
+  db.pg = new metarhia.metasql.Database(options);
 };
