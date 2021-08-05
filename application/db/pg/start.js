e2a6c9158c936db78a728441f027b751e3be225f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-08-05 16:32:09 +0300

    Fix DI console to Database (metasql)

--- application/db/pg/start.js
+++ application/db/pg/start.js
@@ -2,6 +2,6 @@ async () => {
   if (application.worker.id === 'W1') {
     console.debug('Connect to pg');
   }
-  const options = { ...config.database, logger: console };
+  const options = { ...config.database, console };
   db.pg = new metarhia.metasql.Database(options);
 };
