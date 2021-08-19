a9ffbb1c474a748288cc7afee8d503aa85a61a38
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-08-19 23:59:17 +0300

    Update hook example, add redirect

--- application/api/hook.1.js
+++ application/api/hook.1.js
@@ -1,6 +1,9 @@
 ({
   router({ method, args }) {
     console.log({ method, args });
+    if (method === 'favicon') {
+      context.client.redirect('http://127.0.0.1/favicon.png');
+    }
     const result = {};
     return result;
   },
