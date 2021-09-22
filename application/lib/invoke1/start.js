0d71c7445bd0123305ea064bbc37053c153e3794
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-09-22 11:41:44 +0300

    Exclusive thread capture example

--- application/lib/invoke1/start.js
+++ application/lib/invoke1/start.js
@@ -1,11 +1,10 @@
 async () => {
   if (application.worker.id === 'W1') {
-    setTimeout(async () => {
-      const res = await application.invoke({
-        method: 'lib.invoke1.method1',
-        args: { key: 'value' },
-      });
-      console.log('Invoke example', res);
-    }, 1500);
+    const res = await application.invoke({
+      method: 'lib.invoke1.method1',
+      args: { key: 'value' },
+      exclusive: true,
+    });
+    console.log('Invoke example', res);
   }
 };
