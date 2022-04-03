650b7e54ba83fdbb7aaac0af2fc14d892f097eae
Denys Otrishko <shishugi@gmail.com>
2022-04-03 05:53:24 +0300

    Check DB connection status during startup

--- application/db/pg/start.js
+++ application/db/pg/start.js
@@ -4,4 +4,12 @@ async () => {
   }
   const options = { ...config.database, console };
   db.pg = new metarhia.metasql.Database(options);
+
+  // Check that we connected successfully.
+  await db.pg.query('SELECT now()').catch((err) => {
+    if (application.worker.id === 'W1') {
+      console.error(`Failed to setup DB: ${err.message}`);
+    }
+    process.exit(0);
+  });
 };
