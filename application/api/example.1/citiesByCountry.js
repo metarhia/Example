9062a3b4aa4b7df38056801c468b177475c1eaf9
Viacheslav Nikitin <v.v.nikitinnn@gmail.com>
2022-10-04 21:34:17 +0300

    test

--- application/api/example.1/citiesByCountry.js
+++ application/api/example.1/citiesByCountry.js
@@ -2,5 +2,5 @@ async ({ countryId }) => {
   const fields = ['cityId', 'name'];
   const where = { countryId };
   const data = await db.pg.select('City', fields, where);
-  return { result: 'success', data };
+  return { result: 'success1', data };
 };
