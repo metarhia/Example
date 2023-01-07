e7fd9e2a5af4eee40b998252ecec6731623a1084
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-01-07 10:36:49 +0200

    Refresh resmon functionality

---
+++ application/lib/resmon/getStatistics.js
@@ -0,0 +1,7 @@
+async () => {
+  const { heapTotal, heapUsed, external } = process.memoryUsage();
+  const hs = node.v8.getHeapStatistics();
+  const contexts = hs.number_of_native_contexts;
+  const detached = hs.number_of_detached_contexts;
+  return { heapTotal, heapUsed, external, contexts, detached };
+};
