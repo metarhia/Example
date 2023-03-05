e75fb99de9dfb5d67299ab5380b5e400404a2f33
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-03-06 01:28:24 +0200

    Update to be compatible with metacom 3.0.0-alpha.7

--- application/api/example.1/getClientInfo.js
+++ application/api/example.1/getClientInfo.js
@@ -2,7 +2,7 @@
   access: 'public',
   method: async () => {
     const ip = context.client.ip;
-    const { token, accountId } = context;
+    const { token, accountId } = context.session;
     return { result: { ip, token, accountId } };
   },
 });
