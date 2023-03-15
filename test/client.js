1773d81af30110460268adcf925b3efcd6c9a2d2
Leon Polak <lp0808@gmail.com>
2023-03-15 12:56:31 +0200

    Cnanging the test to use the new metacom/lib/client.js

--- test/client.js
+++ test/client.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const runTests = (client, token) => {
+const runTests = (wsClient, wsToken, httpClient, httpToken) => {
   const metatests = require('metatests');
   const fs = require('fs');
   const fsp = fs.promises;
@@ -10,7 +10,7 @@ const runTests = (client, token) => {
 
 
   metatests.testAsync('system/introspect', async (test) => {
-    const introspect = await client.socketCall('system')('introspect')([
+    const introspect = await wsClient.scaffold('system')('introspect')([
       'auth',
       'console',
       'example',
@@ -22,13 +22,13 @@ const runTests = (client, token) => {
   });
 
   metatests.testAsync('example/add', async (test) => {
-    const add = await client.socketCall('example')('add')({ a: 1, b: 2 });
+    const add = await wsClient.scaffold('example')('add')({ a: 1, b: 2 });
     test.strictSame(add, 3);
     test.end();
   });
 
   metatests.testAsync('example/citiesByCountry', async (test) => {
-    const cities = await client.socketCall('example')('citiesByCountry')({
+    const cities = await wsClient.scaffold('example')('citiesByCountry')({
       countryId: 1,
     });
     test.strictEqual(cities?.result, 'success');
@@ -38,7 +38,7 @@ const runTests = (client, token) => {
 
   metatests.testAsync('example/customError', async (test) => {
     try {
-      await client.socketCall('example')('customError')();
+      await wsClient.scaffold('example')('customError')();
     } catch (customError) {
       test.errorCompare(customError, new Error('Return custom error', 12345));
       test.strictEqual(customError?.code, 12345);
@@ -49,7 +49,7 @@ const runTests = (client, token) => {
 
   metatests.testAsync('example/customException', async (test) => {
     try {
-      await client.socketCall('example')('customException')();
+      await wsClient.scaffold('example')('customException')();
     } catch (customError) {
       test.errorCompare(customError, new Error('Custom ecxeption', 12345));
       test.strictEqual(customError?.code, 12345);
@@ -60,7 +60,7 @@ const runTests = (client, token) => {
 
   metatests.testAsync('example/error', async (test) => {
     try {
-      await client.socketCall('example')('error')();
+      await wsClient.scaffold('example')('error')();
     } catch (Error) {
       test.errorCompare(Error, new Error('Return error'));
     } finally {
@@ -69,16 +69,27 @@ const runTests = (client, token) => {
   });
 
   metatests.testAsync('example/exception', async (test) => {
-    await client.socketCall('example')('exception')();
-      test.fail('Shoud never reach here');
+
+    try {
+      await wsClient.scaffold('example')('exception')();
+    }
+    catch(Error){
+      test.errorCompare(Error, new Error('Example exception'));
+    }
+    finally {
       test.end();
+    }
+      // test.fail('Shoud never reach here');
+      // test.end();
   });
 
   metatests.testAsync('example/getClientInfo', async (test) => {
 
-      const info = await client.socketCall('example')('getClientInfo')();
+      const info = await wsClient.scaffold('example')('getClientInfo')();
+
+      // console.log('info', info)
       test.strictEqual(info?.result?.ip, '127.0.0.1');
-      test.strictEqual(info?.result?.token, token);
+      test.strictEqual(info?.result?.token, wsToken);
       test.strictEqual(info?.result?.accountId, '2');
       test.end();
 
@@ -86,11 +97,11 @@ const runTests = (client, token) => {
 
   metatests.testAsync('example/redisSet + redisGet', async (test) => {
     try {
-      const setting = await client.socketCall('example')('redisSet')({
+      const setting = await wsClient.scaffold('example')('redisSet')({
         key: 'MetarhiaExampleTest',
         value: 1,
       });
-      const getting = await client.socketCall('example')('redisGet')({
+      const getting = await wsClient.scaffold('example')('redisGet')({
         key: 'MetarhiaExampleTest',
       });
       // console.log({setting, getting});
@@ -102,17 +113,17 @@ const runTests = (client, token) => {
     }
   });
 
-  metatests.testAsync('example/resources', async (test) => {
-    try {
-      const resources = await client.socketCall('example')('resources')();
-      test.strictEqual(resources?.total, null);
-      // console.log({resources});
-    } catch (Error) {
-      console.log(Error);
-    } finally {
-      test.end();
-    }
-  });
+  // metatests.testAsync('example/resources', async (test) => {
+  //   try {
+  //     const resources = await wsClient.scaffold('example')('resources')();
+  //     test.strictEqual(resources?.total, null);
+  //     // console.log({resources});
+  //   } catch (Error) {
+  //     console.log(Error);
+  //   } finally {
+  //     test.end();
+  //   }
+  // });
 
   //   metatests.testAsync('example/hook', async (test) => {
   //     let hook;
@@ -135,7 +146,7 @@ const runTests = (client, token) => {
 
   // metatests.testAsync('example/subscribe', async (test) => {
   //   try {
-  //     const wait = await client.socketCall('example')('wait')({ delay: 1000 });
+  //     const wait = await wsClient.scaffold('example')('wait')({ delay: 1000 });
   //     // console.log(client.api.chat)
   //     test.strictEqual(wait, 'done');
   //   } catch (Error) {
@@ -162,7 +173,7 @@ const runTests = (client, token) => {
       // const writable = client.createStream(original);
       // readable.pipe(writable);
       // // once readable is piped
-      // await client.socketCall('example')('uploadFile')('sunset_uploaded.jpg', {stremId: writable.streamId});
+      // await wsClient.scaffold('example')('uploadFile')('sunset_uploaded.jpg', {stremId: writable.streamId});
 
       // const fixture = fs.readFileSync(original);
       // const result = fs.readFileSync(uploaded);
@@ -173,7 +184,7 @@ const runTests = (client, token) => {
 
   // metatests.testAsync('example/wait', async (test) => {
   //   try {
-  //     const wait = await client.socketCall('example')('wait')({ delay: 1000 });
+  //     const wait = await wsClient.scaffold('example')('wait')({ delay: 1000 });
   //     test.strictEqual(wait, 'done');
   //   } catch (Error) {
   //     console.log(Error);
@@ -186,20 +197,38 @@ const runTests = (client, token) => {
 
 const connect = async () => {
   const { Metacom } = require('metacom/lib/client');
-  const url = 'ws://127.0.0.1:8000/api';
-  const client = Metacom.create(url);
-  await client.ready();
-  const signin = await client.socketCall('auth')('signin')({
+
+  const wsUrl = 'ws://127.0.0.1:8000/api';
+  const wsClient = Metacom.create(wsUrl);
+  await wsClient.open();
+  const wsSignin = await wsClient.scaffold('auth')('signin')({
     login: 'marcus',
     password: 'marcus',
   });
-  const token = signin?.token;
+
+  const wsToken = wsSignin?.token;
+  // const wsClient = null, wsToken = null;
+
+  const httpUrl = 'http://127.0.0.1:8000/api';
+  const httpClient = Metacom.create(httpUrl);
+  await httpClient.open();
+  const httpSignin = await httpClient.scaffold('auth')('signin')({
+    login: 'marcus',
+    password: 'marcus',
+  });
+  const httpToken = httpSignin?.token;
+  //  const httpClient = null, httpToken = null;
+
+
   // await client.load( 'auth',
   // 'console',
   // 'example',
   // 'files',
   // 'test');
-  runTests(client, token);
+
+
+
+  runTests(wsClient, wsToken, httpClient, httpToken);
 
 };
 
