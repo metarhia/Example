d7ef10c6aaaa81d02dafdd1094637f8c654d4021
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-04-06 07:09:26 +0300

    Handle startup error to prevent app termination

--- application/domain/time/start.js
+++ application/domain/time/start.js
@@ -1,11 +1,17 @@
 async () => {
   if (application.worker.id === 'W1') {
-    setTimeout(async () => {
-      const time = await bus.worldTime.currentTime({
-        area: 'Europe',
-        location: 'Kiev',
-      });
-      console.log(`${time.timezone} - ${time.datetime}`);
+    setTimeout(() => {
+      bus.worldTime
+        .currentTime({
+          area: 'Europe',
+          location: 'Kiev',
+        })
+        .then((time) => {
+          console.log(`${time.timezone} - ${time.datetime}`);
+        })
+        .catch(() => {
+          console.log('Can not access time server');
+        });
     }, 1000);
   }
 };
