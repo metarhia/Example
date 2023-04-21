65ed13691e7204086bd3477368f3bff754d4e1e3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-04-22 00:45:27 +0300

    Apply prettier fixes

--- application/domain/math/start.js
+++ application/domain/math/start.js
@@ -1,10 +1,12 @@
 async () => {
   if (application.worker.id === 'W1') {
     setTimeout(async () => {
-      const res = await bus.math.eval({
-        expr: '2+3*sqrt(4)',
-        precision: 3,
-      }).catch(() => 'Can not access math server');
+      const res = await bus.math
+        .eval({
+          expr: '2+3*sqrt(4)',
+          precision: 3,
+        })
+        .catch(() => 'Can not access math server');
       console.log({ math: res });
     }, 1000);
   }
--- application/domain/time/start.js
+++ application/domain/time/start.js
@@ -1,14 +1,17 @@
 async () => {
   if (application.worker.id === 'W1') {
     setTimeout(() => {
-      bus.worldTime.currentTime({
-        area: 'Europe',
-        location: 'Kiev',
-      }).then((time) => {
-        console.log(`${time.timezone} - ${time.datetime}`);
-      }).catch(() => {
-        console.log('Can not access time server');
-      });
+      bus.worldTime
+        .currentTime({
+          area: 'Europe',
+          location: 'Kiev',
+        })
+        .then((time) => {
+          console.log(`${time.timezone} - ${time.datetime}`);
+        })
+        .catch(() => {
+          console.log('Can not access time server');
+        });
     }, 1000);
   }
 };
