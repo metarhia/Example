336cca8e64369fb3c87dc3063477efd5dc221c0a
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-05-05 14:24:02 +0300

    Apply suggestions from code review

--- application/lib/task1/start.js
+++ application/lib/task1/start.js
@@ -1,13 +1,14 @@
 async () => {
+  if (application) return; // comment to enable
   if (application.worker.id === 'W1') {
-    // setTimeout(async () => {
-    //   const res = await application.scheduler.add({
-    //     name: 'name',
-    //     every: 'Sep 10th 10s',
-    //     args: { i: 2 },
-    //     run: 'lib.task1.f1',
-    //   });
-    //   console.log('Add task', res);
-    // }, 1000);
+    setTimeout(async () => {
+      const res = await application.scheduler.add({
+        name: 'name',
+        every: 'Sep 10th 10s',
+        args: { i: 2 },
+        run: 'lib.task1.f1',
+      });
+      console.log('Add task', res);
+    }, 1000);
   }
 };
