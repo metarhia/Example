0640b455df2fdd4f9e8b53106725e5d7b8367f6d
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-05-06 12:36:06 +0300

    Create config to enable/disable examples

--- application/lib/invoke1/start.js
+++ application/lib/invoke1/start.js
@@ -1,10 +1,10 @@
 async () => {
-  if (application.worker.id === 'W1') {
-    const res = await application.invoke({
-      method: 'lib.invoke1.method1',
-      args: { key: 'value' },
-      exclusive: true,
-    });
-    console.log('Invoke example', res);
-  }
+  if (!config.examples.invoke) return;
+  if (application.worker.id !== 'W1') return;
+  const res = await application.invoke({
+    method: 'lib.invoke1.method1',
+    args: { key: 'value' },
+    exclusive: true,
+  });
+  console.log('Invoke example', res);
 };
