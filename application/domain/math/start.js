7dcdc505c27f95511de84c44f510a663af6a014d
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-05-06 12:36:06 +0300

    Create config to enable/disable examples

--- application/domain/math/start.js
+++ application/domain/math/start.js
@@ -1,13 +1,13 @@
 async () => {
-  if (application.worker.id === 'W1') {
-    setTimeout(async () => {
-      const res = await bus.math
-        .eval({
-          expr: '2+3*sqrt(4)',
-          precision: 3,
-        })
-        .catch(() => 'Can not access math server');
-      console.log({ math: res });
-    }, 1000);
-  }
+  if (!config.examples.bus) return;
+  if (application.worker.id !== 'W1') return;
+  setTimeout(async () => {
+    const res = await bus.math
+      .eval({
+        expr: '2+3*sqrt(4)',
+        precision: 3,
+      })
+      .catch(() => 'Can not access math server');
+    console.log({ math: res });
+  }, 1000);
 };
--- application/domain/time/start.js
+++ application/domain/time/start.js
@@ -1,17 +1,17 @@
 async () => {
-  if (application.worker.id === 'W1') {
-    setTimeout(() => {
-      bus.worldTime
-        .currentTime({
-          area: 'Europe',
-          location: 'Kiev',
-        })
-        .then((time) => {
-          console.log(`${time.timezone} - ${time.datetime}`);
-        })
-        .catch(() => {
-          console.log('Can not access time server');
-        });
-    }, 1000);
-  }
+  if (!config.examples.bus) return;
+  if (application.worker.id !== 'W1') return;
+  setTimeout(() => {
+    bus.worldTime
+      .currentTime({
+        area: 'Europe',
+        location: 'Kiev',
+      })
+      .then((time) => {
+        console.log(`${time.timezone} - ${time.datetime}`);
+      })
+      .catch(() => {
+        console.log('Can not access time server');
+      });
+  }, 1000);
 };
