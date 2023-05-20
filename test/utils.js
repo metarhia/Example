476b3b055eaa47c9c2ba6a41307795b2dd6fe258
Heorhii <margievge@gmail.com>
2023-05-20 22:52:31 +0200

    fix get

--- test/utils.js
+++ test/utils.js
@@ -20,42 +20,40 @@ const delay = async (ms) => new Promise((res) => setTimeout(res, ms));
 
 const testHook = async ({ url, path, argsString }) => {
   return new Promise((resolve, reject) => {
-    http
-      .get(url + path + '?' + argsString, { method: 'POST' }, (res) => {
-        const { statusCode } = res;
-        const contentType = res.headers['content-type'];
-        let error;
-        // Any 2xx status code signals a successful response but
-        // here we're only checking for 200.
-        if (statusCode !== 200) {
-          error = new Error(`Request Failed.\n Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType)) {
-          error = new Error(
-            'Invalid content-type.\n' +
-              `Expected application/json but received ${contentType}`,
-          );
-        }
-        if (error) {
-          // Consume response data to free up memory
-          res.resume();
-          reject(error);
-        }
+    http.get(url + path + '?' + argsString, { method: 'POST' }, (res) => {
+      const { statusCode } = res;
+      const contentType = res.headers['content-type'];
+      let error;
+      // Any 2xx status code signals a successful response but
+      // here we're only checking for 200.
+      if (statusCode !== 200) {
+        error = new Error(`Request Failed.\n Status Code: ${statusCode}`);
+      } else if (!/^application\/json/.test(contentType)) {
+        error = new Error(
+          'Invalid content-type.\n' +
+            `Expected application/json but received ${contentType}`,
+        );
+      }
+      if (error) {
+        // Consume response data to free up memory
+        res.resume();
+        reject(error);
+      }
 
-        res.setEncoding('utf8');
-        let rawData = '';
-        res.on('data', (chunk) => {
-          rawData += chunk;
-        });
-        res.on('end', () => {
-          try {
-            const parsedData = JSON.parse(rawData);
-            resolve(parsedData);
-          } catch (err) {
-            reject(err);
-          }
-        });
-      })
-      .catch(console.log);
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', (chunk) => {
+        rawData += chunk;
+      });
+      res.on('end', () => {
+        try {
+          const parsedData = JSON.parse(rawData);
+          resolve(parsedData);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
   });
 };
 
