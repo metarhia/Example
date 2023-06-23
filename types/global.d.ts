f779ba6e61a7c9c95a6a2c689fd9d851d0ea2628
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-06-23 10:53:30 +0300

    Error handling proposal

--- types/global.d.ts
+++ types/global.d.ts
@@ -23,3 +23,27 @@ declare global {
     const pg: Database;
   }
 }
+
+export interface ErrorOptions {
+  code?: number | string;
+  cause?: Error;
+}
+
+export class Error extends global.Error {
+  constructor(message: string, options?: number | string | ErrorOptions);
+  message: string;
+  stack: string;
+  code?: number | string;
+  cause?: Error;
+}
+
+type Errors = Record<string, string>;
+
+export class DomainError extends Error {
+  constructor(code?: string, options?: number | string | ErrorOptions);
+  message: string;
+  stack: string;
+  code?: number | string;
+  cause?: Error;
+  toError(errors: Errors): Error;
+}
