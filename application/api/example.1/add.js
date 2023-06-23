dc5ea112eb70c83d747d4cc16a2b9dcb5b4a3e26
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-06-23 10:53:30 +0300

    Error handling approach in Metarhia 3

--- application/api/example.1/add.js
+++ application/api/example.1/add.js
@@ -1,13 +1,24 @@
 ({
+  access: 'public',
+
   parameters: {
     a: 'number',
     b: 'number',
   },
 
   method: async ({ a, b }) => {
+    if (a < 0) return new DomainError('EARGA');
+    if (b > 500) return new DomainError('EARGB');
+    if (Number.isNaN(a)) throw Error('Not a number: a');
+    if (Number.isNaN(b)) throw Error('Not a number: b');
     const result = a + b;
     return result;
   },
 
   returns: 'number',
+
+  errors: {
+    EARGA: 'Invalid argument: "a" expected to be > 0',
+    EARGB: 'Invalid argument: "b" expected to be < 500',
+  },
 });
