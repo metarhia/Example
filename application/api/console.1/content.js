596dc00498f17c5873d79177262a80f507258bfa
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-09-12 21:55:02 +0300

    Update content to support new Cache format

--- application/api/console.1/content.js
+++ application/api/console.1/content.js
@@ -4,9 +4,9 @@
   async method({ name }) {
     // Try type: new api.console.content.CustomError('EPARSE');
     const filePath = `/content/${name}.md`;
-    const buffer = application.resources.get(filePath);
-    if (!buffer) return new Error('Content is not found');
-    return { text: buffer.toString() };
+    const file = application.resources.get(filePath);
+    if (!file) return new Error('Content is not found');
+    return { text: file.data.toString() };
   },
 
   CustomError: class CustomError extends Error {},
