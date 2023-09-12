387ccff2d77405275c5f4e0e2b863739efeaf58a
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-09-12 21:54:04 +0300

    Change relative urls to absolute

--- application/static/console.js
+++ application/static/console.js
@@ -1,4 +1,4 @@
-import { Metacom } from './metacom.js';
+import { Metacom } from '/metacom.js';
 
 const ALPHA_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const ALPHA_LOWER = 'abcdefghijklmnopqrstuvwxyz';
