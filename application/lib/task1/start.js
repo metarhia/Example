3659185b527b2c4ef453c900288adcf85d4221f5
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-10-24 20:56:43 +0300

    Optimize tests and remove unneeded

--- application/lib/task1/start.js
+++ application/lib/task1/start.js
@@ -1,13 +1,11 @@
 async () => {
   if (!config.examples.scheduler) return;
   if (application.worker.id !== 'W1') return;
-  setTimeout(async () => {
-    const res = await application.scheduler.add({
-      name: 'name',
-      every: 'Sep 10th 10s',
-      args: { i: 2 },
-      run: 'lib.task1.f1',
-    });
-    console.log('Add task', res);
-  }, 1000);
+  const res = await application.scheduler.add({
+    name: 'name',
+    every: 'Oct 19th 10s',
+    args: { i: 2 },
+    run: 'lib.task1.f1',
+  });
+  console.log('Add task', res);
 };
