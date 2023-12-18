af30adf6b5623a4aa4798d73ff8787c6850d7560
Vladyslav Karpenko <klarpen@gmail.com>
2023-12-18 15:32:39 +0200

    Wrap `signin` test result assertions with named test case
    PR-URL: https://github.com/metarhia/Example/pull/264

--- application/domain/tests/api.js
+++ application/domain/tests/api.js
@@ -1,12 +1,15 @@
 ({
   async cases(t, metacom) {
     if (!t) return;
-    const res = await metacom.api.auth.signin({
-      login: 'marcus',
-      password: 'marcus',
+
+    await t.test('Sign into session', async () => {
+      const res = await metacom.api.auth.signin({
+        login: 'marcus',
+        password: 'marcus',
+      });
+      node.assert.strictEqual(res.status, 'logged');
+      node.assert.strictEqual(typeof res.token, 'string');
     });
-    node.assert.strictEqual(res.status, 'logged');
-    node.assert.strictEqual(typeof res.token, 'string');
 
     await t.test(`Call example.add({ a, b })`, async () => {
       const res = await metacom.api.example.add({ a: 10, b: 20 });
