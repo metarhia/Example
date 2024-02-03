2f45d2e047619d43cd3953c1971da938b51a52fc
ihorkomakhwork <I.Komakh.FIT.122.20@knute.edu.ua>
2024-02-03 15:19:07 +0200

    Remove unnecessary arg

--- application/api/auth/register.js
+++ application/api/auth/register.js
@@ -1,8 +1,8 @@
 ({
   access: 'public',
-  method: async ({ login, password, fullName }) => {
+  method: async ({ login, password }) => {
     const hash = await metarhia.metautil.hashPassword(password);
-    await api.auth.provider.registerUser(login, hash, fullName);
+    await api.auth.provider.registerUser(login, hash);
     const token = await context.client.startSession();
     return { status: 'success', token };
   },
